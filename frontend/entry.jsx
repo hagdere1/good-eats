43eb8b7c956b03e97eb72f8c73074f41ca991656
Haig Gregory Agdere <hagdere1@binghamton.edu>
2016-01-26 18:45:43 -0500

    Configure package.json dependencies.

---
+++ frontend/entry.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
+});
